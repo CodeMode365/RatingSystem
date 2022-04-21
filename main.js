bcbd4265271f962de7ad207b6473a67e0e58cc2d
CodeMode365 <102400760+CodeMode365@users.noreply.github.com>
2022-04-21 09:57:00 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+/*
 let star1 = document.getElementById('star1');
 let star2 = document.getElementById('star2');
 let star3 = document.getElementById('star3');
 let star4 = document.getElementById('star4');
 let star5 = document.getElementById('star5');
+*/
 let emoji =document.getElementsByClassName('far');
 
 const stars= document.querySelectorAll('.fa-star');
